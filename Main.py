0e434ed5aba6b1424405583c9645358b12045455
nvkulkarni21 <30701947+nvkulkarni21@users.noreply.github.com>
2017-09-09 17:41:05 +0530

    Update Main.py
    After complete working code at 5.35 PM saturday hackathon day

--- Main.py
+++ Main.py
@@ -14,29 +14,14 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-
 sid = SentimentIntensityAnalyzer()
 
-    
-vaderarray = []
-
-def vader(sent):
-    #print ("In Vader function.. ")
-    sentvader= sent
-    sid = SentimentIntensityAnalyzer()
-    ss = (sid.polarity_scores(sentvader))
-    
-    for k in sorted(ss):
-         #print ( k )
-         #print  ( ss[k] )
-         #print  (k, ss[k])
-         vaderarray.append([k, ss[k]])
-    #print (*vaderarray)
-    return(vaderarray)
-
-
-vaderres=""
 sentires=""
+comp_cntV=0
+pos_cntV=0
+neg_cntV=0
+neu_cntV=0
+sentcnt=0
 
 pos_cnt=0
 neg_cnt=0
@@ -55,11 +40,11 @@ for root, dirs, filenames in os.walk(indir):
                     sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
                     #print ( sentences )
                     for line in sentences:
-
+                        
                         strippedString = line.strip()
                         if strippedString == "":
                             break
-                                                    
+                        sentcnt = sentcnt + 1                            
                         print ( strippedString )
                         #vaderres = vader( line )
                         sentires = sentiment(strippedString)
@@ -71,32 +56,29 @@ for root, dirs, filenames in os.walk(indir):
 
                         ss = (sid.polarity_scores(strippedString))    
                         for k in sorted(ss):
-                             print  ( k , ss[k] )
- 
+                            print (k, ss[k])
+                            if k == "compound":
+                                comp_cntV=ss[k]+comp_cntV
+                            if k == "neg":
+                                neg_cntV=ss[k]+neg_cntV
+                            if k == "pos":
+                                pos_cntV=ss[k]+pos_cntV
+                            if k == "neu":
+                                neu_cntV=ss[k]+neu_cntV
+                        
+
+    print ( "**************************************************************************************************************************************************" )
+    print ( "Output for Vader statistics " )
+    print ( "Avg Compound occurrence : " + str( comp_cntV /sentcnt )   )
+    print ( "Avg Negative occurrence : " + str( neg_cntV / sentcnt)   )
+    print ( "Avg Positive occurrence : " + str( pos_cntV /sentcnt )   )
+    print ( "Avg Neutral occurrence : "  + str( neu_cntV / sentcnt )   )   
+    print ( "**************************************************************************************************************************************************" )
+
+    
     print ( "**************************************************************************************************************************************************" )
     print ( "Output for Naive bayes statistical machine learning statistical algorithm " )
     print ( "Positive occurrence : " + str( pos_cnt )   )
     print ( "Negative occurrence : " + str( neg_cnt )   )       
     print ( "As per Naive bayes statistical machine learning statistical algorithm postive to negative sentiment ratio is : " + str( pos_cnt / neg_cnt ) )           
     print ( "**************************************************************************************************************************************************" )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                        
-
-
-
-
-
-    
